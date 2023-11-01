17aad3c7190bb7219188a4eeedc3fc3489b89578
Josef Friedrich <josef@friedrich.rocks>
2023-11-01 11:46:24 +0100

    Add hello world

---
+++ src/main/java/rocks/friedrich/litiengine/Program.java
@@ -0,0 +1,11 @@
+package rocks.friedrich.litiengine;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+
+  public static void main(String[] args) {
+    Game.init(args);
+    Game.start();
+  }
+}
