acc407298cffae4b06720d51b13cd4a69c239983
Josef Friedrich <josef@friedrich.rocks>
2024-04-19 17:18:14 +0200

    Upgrade litiengine

--- src/main/java/rocks/friedrich/litiengine/Program.java
+++ src/main/java/rocks/friedrich/litiengine/Program.java
@@ -5,6 +5,10 @@ import de.gurkenlabs.litiengine.Game;
 public class Program {
 
   public static void main(String[] args) {
+    Game.info().setName("Example Game");
+    Game.info().setSubTitle("tested by Josef Friedrich");
+    Game.info().setVersion("v0.1.0");
+    Game.info().setWebsite("https://github.com/Josef-Friedrich/litiengine-examples");
     Game.init(args);
     Game.start();
   }
