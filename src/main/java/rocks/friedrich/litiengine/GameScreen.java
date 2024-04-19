4792647fc08f63c18598638959f07859a4e74268
Josef Friedrich <josef@friedrich.rocks>
2024-04-19 18:35:07 +0200

    Add GameScreen

---
+++ src/main/java/rocks/friedrich/litiengine/GameScreen.java
@@ -0,0 +1,26 @@
+package rocks.friedrich.litiengine;
+
+import de.gurkenlabs.litiengine.Game;
+
+import de.gurkenlabs.litiengine.gui.screens.Screen;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+public class GameScreen extends Screen {
+    public GameScreen() {
+        super("game");
+    }
+
+    @Override
+    public void prepare() {
+        super.prepare();
+        Game.window().getRenderComponent().setBackground(Color.decode("#ff0000"));
+    }
+
+    public void render(Graphics2D g) {
+        super.render(g);
+        Game.graphics().renderText(g, "Hello, LITIengine!", 100, 100);
+
+    }
+
+}
--- src/main/java/rocks/friedrich/litiengine/Program.java
+++ src/main/java/rocks/friedrich/litiengine/Program.java
@@ -1,6 +1,7 @@
 package rocks.friedrich.litiengine;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.gui.screens.Resolution;
 
 public class Program {
 
@@ -10,6 +11,9 @@ public class Program {
     Game.info().setVersion("v0.1.0");
     Game.info().setWebsite("https://github.com/Josef-Friedrich/litiengine-examples");
     Game.init(args);
+    Game.window().setResolution(Resolution.Ratio4x3.RES_1024x768);
+
+    Game.screens().add(new GameScreen());
     Game.start();
   }
 }
